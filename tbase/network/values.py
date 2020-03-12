5f160d8ce8f6a0c7d0e71394eb0f2bbee084db32
iminders <liuwen.w@qq.com>
2020-03-12 17:38:21 +0800

    net

--- tbase/network/values.py
+++ tbase/network/values.py
@@ -115,17 +115,17 @@ def get_single_value_net(env, args):
     if args.value_net == "LSTM_Merge_MLP":
         return LSTM_Merge_MLP(
             device=device,
-            seq_len=seq_len, obs_input_size=input_size, rnn_hidden_size=300,
+            seq_len=seq_len, obs_input_size=input_size, rnn_hidden_size=50,
             num_layers=1, dropout=0.0, learning_rate=args.lr,
             act_input_size=act_size,
-            act_fc1_size=200, act_fc2_size=100, output_size=1,
+            act_fc1_size=50, act_fc2_size=10, output_size=1,
             activation=activation).to(device)
     if args.value_net == "LSTM_MLP_A2C":
         return LSTM_MLP_A2C(
             device=device,
-            seq_len=seq_len, input_size=input_size, hidden_size=300,
+            seq_len=seq_len, input_size=input_size, hidden_size=50,
             num_layers=1, dropout=0.0, learning_rate=args.lr,
-            fc_size=200, output_size=1,
+            fc_size=30, output_size=1,
             activation=activation).to(device)
     else:
         raise ValueError("Not implement value_net: %s" % args.value_net)
